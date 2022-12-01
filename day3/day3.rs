d3c02977a460a56b06ef8ae09f31ada6c513ec1c
voidboy <aurelienclerac@pm.me>
2022-12-01 16:27:39 +0100

    day3, part 2

--- day3/day3.rs
+++ day3/day3.rs
@@ -1,28 +1,47 @@
 use std::fs;
 
+fn koh_lanta(mut equipe_jaune: Vec<&str>, most: bool) -> &str {
+   let len = equipe_jaune[0].len();
+   for i in 0..len {
+       let slice_len = equipe_jaune.len();
+       if slice_len == 1 { break ; }
+       let mut ones = 0;
+       for value in &equipe_jaune {
+           if value.as_bytes()[i] == b'1' { ones += 1; }
+       }
+       equipe_jaune = equipe_jaune
+          .into_iter()
+          .filter(|value| {
+              if ones >= slice_len / 2 {
+                  if most {
+                      value.as_bytes()[i] == b'1'
+                  } else {
+                      value.as_bytes()[i] == b'0'
+                  }
+              } else {
+                  if most {
+                      value.as_bytes()[i] == b'0'
+                  } else {
+                      value.as_bytes()[i] == b'1'
+                  }
+              }
+           })
+          .collect();
+   }
+   equipe_jaune[0]
+}
+
 fn main() {
     let contents = fs::read_to_string("input")
         .expect("Cannot read the file");
 
-   let mut ones: usize;
-   let bits = contents.lines().collect::<Vec<&str>>();
-   let mut gamma_rate = String::new();
-   let mut epsilon_rate = String::new();
-   for bit in 0..bits[0].len() {
-       ones = 0;
-       for value in &bits {
-           if value.as_bytes()[bit] == b'1' { ones += 1; }
-       }
-       if ones >= bits.len() / 2
-       {
-           gamma_rate.push('1');
-           epsilon_rate.push('0');
-       } else {
-           gamma_rate.push('0');
-           epsilon_rate.push('1');
-       }
-   }
-   println!("diagnostic: {}",
-       usize::from_str_radix(&gamma_rate, 2).unwrap() *
-       usize::from_str_radix(&epsilon_rate, 2).unwrap());
+   let oxygen_generator_rating = contents
+       .lines()
+       .collect::<Vec<&str>>();
+   let co2_scrubber_rating = contents
+       .lines()
+       .collect::<Vec<&str>>();
+   println!("magic number is {}", 
+       usize::from_str_radix(koh_lanta(oxygen_generator_rating, true), 2).unwrap() *
+       usize::from_str_radix(koh_lanta(co2_scrubber_rating, false), 2).unwrap());
 }
